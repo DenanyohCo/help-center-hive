6fb50a1672c570ce71c350c30461b1cffaa893b9
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-02-19 19:16:20 +0530

    Update next.config.mjs with new domain for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-    unoptimized: true,
-    domains: ['localhost', {vercel.app, railway.app}],
-    loader: 'imgix',
-    path: './media',
+        unoptimized: true,
+        domains: ['localhost', 'help-center-hive-production.up.railway.app'],
+        loader: 'imgix',
+        path: './media',
     },
-    };
-    
-    export default nextConfig;
+};
+
+export default nextConfig;
