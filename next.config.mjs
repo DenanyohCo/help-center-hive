43e71c5853bd049b15c94e6e1954ef4e5a8a30a5
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-03-04 20:12:18 +0530

    Update image loader and path in next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
     images: {
         unoptimized: true,
         domains: ['localhost', 'https://www.helpcenter.design'],
-        // loader: 'imgix',
-        // path: './media',
+        loader: 'imgix',
+        path: '../media',
     },
 };
 
