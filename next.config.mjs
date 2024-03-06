d674d2858098441230d8462aae1981605807989d
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-03-06 23:50:04 +0530

    Update next.config.mjs to include remotePatterns for image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        // unoptimized: true,
-        domains: ['localhost', 'https://www.helpcenter.design'],
-        loader: 'imgix',
-        path: '../media',
+        unoptimized: true,
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "localhost",
+            },
+            {
+                protocol: "https",
+                hostname: "www.helpcenter.design",
+            },
+        ],
     },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
