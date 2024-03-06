76bcd442149846b1b7450f3cfbf5df3c6f1b4faf
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-03-06 21:38:02 +0530

    Remove access control from Media collection

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -5,15 +5,6 @@ export const Media: CollectionConfig = {
     admin: {
         useAsTitle: "alt",
     },
-    access: {
-        read: () => true,
-        create: () => true,
-        update: () => true,
-        delete: () => true,
-        // create: ({ req }) => req.user.email === process.env.ADMIN_EMAIL,
-        // update: ({ req }) => req.user.email === process.env.ADMIN_EMAIL,
-        // delete: ({ req }) => req.user.email === process.env.ADMIN_EMAIL,
-    },
     upload: {
         staticURL: "/media",
         staticDir: "media",
