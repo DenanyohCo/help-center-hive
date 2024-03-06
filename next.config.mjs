5ee83dc3872cf948d278330df755081a2bab3025
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-03-06 23:52:35 +0530

    Commented out unoptimized image configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        unoptimized: true,
-        remotePatterns: [
-            {
-                protocol: "http",
-                hostname: "localhost",
-            },
-            {
-                protocol: "https",
-                hostname: "www.helpcenter.design",
-            },
-        ],
-    },
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         unoptimized: true,
+//         remotePatterns: [
+//             {
+//                 protocol: "http",
+//                 hostname: "localhost",
+//             },
+//             {
+//                 protocol: "https",
+//                 hostname: "www.helpcenter.design",
+//             },
+//         ],
+//     },
+// };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
