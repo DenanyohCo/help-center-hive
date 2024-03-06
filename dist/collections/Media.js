d647b0269ee6a0bb80c81403aa6240dec6637808
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-03-07 00:36:09 +0530

    Refactor Media access control and update next.config.mjs

--- dist/collections/Media.js
+++ dist/collections/Media.js
@@ -6,21 +6,6 @@ exports.Media = {
     admin: {
         useAsTitle: "alt",
     },
-    access: {
-        read: function () { return true; },
-        create: function (_a) {
-            var req = _a.req;
-            return req.user.email === process.env.ADMIN_EMAIL;
-        },
-        update: function (_a) {
-            var req = _a.req;
-            return req.user.email === process.env.ADMIN_EMAIL;
-        },
-        delete: function (_a) {
-            var req = _a.req;
-            return req.user.email === process.env.ADMIN_EMAIL;
-        },
-    },
     upload: {
         staticURL: "/media",
         staticDir: "media",
