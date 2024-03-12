575f3a338e10a1664816e7951b0c9a33d0fd96ee
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-03-12 19:59:11 +0530

    Add reactStrictMode to next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
         loader: 'imgix',
         path: '../media',
     },
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/help-center-hive' : '',
+    reactStrictMode: true,
 };
 
 export default nextConfig;
