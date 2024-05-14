393fa64889f9ae949920da58eadc2bd719452437
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-05-14 22:16:47 +0530

    chore: Remove unused files and configurations

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['localhost', 'helpcenter.design'],
-        loader: 'imgix',
-        path: './media',
-    },
-};
+const nextConfig = {};
+
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,103 +1,20 @@
 import type { Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
-const config: import("tailwindcss").Config = {
-    darkMode: ["class"],
-    content: [
-        "./pages/**/*.{ts,tsx}",
-        "./components/**/*.{ts,tsx}",
-        "./app/**/*.{ts,tsx}",
-        "./src/**/*.{ts,tsx}",
-    ],
-    prefix: "",
-    theme: {
-        screens: {
-            sm: "640px",
-            // => @media (min-width: 640px) { ... }
 
-            md: "768px",
-            // => @media (min-width: 768px) { ... }
-
-            lg: "1024px",
-            // => @media (min-width: 1024px) { ... }
-
-            xl: "1280px",
-            // => @media (min-width: 1280px) { ... }
-
-            "2xl": "1536px",
-            // => @media (min-width: 1536px) { ... }
-        },
-        container: {
-            center: true,
-            padding: {
-                DEFAULT: "2rem",
-                lg: "4rem",
-            },
-            screens: {
-                lg: "1024px",
-                "2xl": "1536px",
-            },
-        },
-        extend: {
-            fontFamily: {
-                sans: ["var(--font-sans)", ...fontFamily.sans],
-            },
-            colors: {
-                border: "hsl(var(--border))",
-                input: "hsl(var(--input))",
-                ring: "hsl(var(--ring))",
-                background: "hsl(var(--background))",
-                foreground: "hsl(var(--foreground))",
-                primary: {
-                    DEFAULT: "hsl(var(--primary))",
-                    foreground: "hsl(var(--primary-foreground))",
-                },
-                secondary: {
-                    DEFAULT: "hsl(var(--secondary))",
-                    foreground: "hsl(var(--secondary-foreground))",
-                },
-                destructive: {
-                    DEFAULT: "hsl(var(--destructive))",
-                    foreground: "hsl(var(--destructive-foreground))",
-                },
-                muted: {
-                    DEFAULT: "hsl(var(--muted))",
-                    foreground: "hsl(var(--muted-foreground))",
-                },
-                accent: {
-                    DEFAULT: "hsl(var(--accent))",
-                    foreground: "hsl(var(--accent-foreground))",
-                },
-                popover: {
-                    DEFAULT: "hsl(var(--popover))",
-                    foreground: "hsl(var(--popover-foreground))",
-                },
-                card: {
-                    DEFAULT: "hsl(var(--card))",
-                    foreground: "hsl(var(--card-foreground))",
-                },
-            },
-            borderRadius: {
-                lg: "var(--radius)",
-                md: "calc(var(--radius) - 2px)",
-                sm: "calc(var(--radius) - 4px)",
-            },
-            keyframes: {
-                "accordion-down": {
-                    from: { height: "0" },
-                    to: { height: "var(--radix-accordion-content-height)" },
-                },
-                "accordion-up": {
-                    from: { height: "var(--radix-accordion-content-height)" },
-                    to: { height: "0" },
-                },
-            },
-            animation: {
-                "accordion-down": "accordion-down 0.2s ease-out",
-                "accordion-up": "accordion-up 0.2s ease-out",
-            },
-        },
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
     },
-    plugins: [require("tailwindcss-animate")],
-} satisfies Config;
-
+  },
+  plugins: [],
+};
 export default config;
