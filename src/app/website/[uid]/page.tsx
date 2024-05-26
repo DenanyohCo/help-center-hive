c1a5cb726b20180112a3eb59f6ce10777d85502a
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-05-26 19:27:56 +0530

    refactor: Update PrivacyPolicyDocument type definition and add PrivacyPolicySlice component

---
+++ src/app/website/[uid]/page.tsx
@@ -0,0 +1,26 @@
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+import { MdArrowBackIos } from "react-icons/md";
+import { PiArrowSquareOutFill } from "react-icons/pi";
+import React, { useEffect } from "react";
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+const SitePage = () => {
+    return (
+        <section className="container">
+            <Link href={"/"} className="flex items-center py-4">
+                <MdArrowBackIos /> See All Inspiration
+            </Link>
+
+        </section>
+    );
+};
+
+export default SitePage;
--- src/slices/PrivacyPolicy/index.tsx
+++ src/slices/PrivacyPolicy/index.tsx
@@ -16,14 +16,15 @@ const PrivacyPolicy = ({ slice }: PrivacyPolicyProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       className="container py-12 my-4"
-    >                <div className="container bg-[#FFFFFF]">
-        <div className="text-6xl py-4 font-bold">{slice.primary.privacy_policy_heading}</div>
-        <div className="text-lg">Last updated on {slice.primary.last_updated_on}</div>
-        <div className="text-4xl font-bold pt-12 mt-4">
-          <PrismicRichText field={slice.primary.privacy_policy_text} />
+    >
+      <div className="container bg-[#FFFFFF] m-4">
+        <div className="my-4">
+          <div className="text-6xl py-4 font-bold">{slice.primary.privacy_policy_heading}</div>
+          <div className="text-lg">Last updated on {slice.primary.last_updated_on}</div>
         </div>
+        <PrismicRichText field={slice.primary.privacy_policy_text} />
       </div>
-    </section>
+    </section >
   );
 };
 
